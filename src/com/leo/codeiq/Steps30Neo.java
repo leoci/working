9f55421ebe4cf2c899d53620f919c810767bfb8d
LEO <itaru1725@gmail.com>
2015-02-05 04:25:18 +0900

    add preparation

--- src/com/leo/codeiq/Steps30Neo.java
+++ src/com/leo/codeiq/Steps30Neo.java
@@ -1,12 +1,14 @@
 package com.leo.codeiq;
 
-public class Steps30 {
+import java.util.stream.IntStream;
+
+public class Steps30Neo {
 
 	public static void main(String[] args) {
 		long millis = System.currentTimeMillis();
 
 		PatternCounter pc = new PatternCounter();
-		long answer = pc.count(30);
+		int answer = pc.count(30);
 
 		System.out.println(answer);
 		System.out.println(System.currentTimeMillis() - millis);
@@ -17,13 +19,23 @@ public class Steps30 {
 		private static final int MIN_HOP = 1;
 		private static final int MAX_HOP = 3;
 
-		public long count(int steps) {
+		private int CACHE_THRESHOLD = 10;
+		private int[] cachedCount;
+
+		public PatternCounter() {
+			cachedCount = IntStream.rangeClosed(1, CACHE_THRESHOLD)
+					.map(this::count).toArray();
+		}
+
+		public int count(int steps) {
 			return step(steps, 0);
 		}
 
-		private long step(int rest, long count) {
+		private int step(int rest, int count) {
 			if (rest == 0) {
 				return ++count;
+			} else if (cachedCount != null && rest <= CACHE_THRESHOLD) {
+				return count + cachedCount[rest - 1];
 			}
 			for (int i = MIN_HOP; i <= Math.min(MAX_HOP, rest); i++) {
 				count = step(rest - i, count);
